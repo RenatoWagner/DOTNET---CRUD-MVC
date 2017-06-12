d7aa412a717798aa84215c9c68065a719347158b
Renato Wagner <renatowagnerx@gmail.com>
2017-06-12 18:59:54 -0300

    Atividade DOTNET

---
+++ umParaMuitos/Models/Atividade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace umParaMuitos.Models
+{
+    public class Atividade
+    {
+        public int AtividadeID { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime Final { get; set; }
+        public string  Descricao { get; set; }
+        public string  Local { get; set; }
+
+        public int PessoaID { get; set; }
+        public virtual Pessoa Pessoa { get; set; } 
+
+    }
+}
---
+++ umParaMuitos/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace umParaMuitos.Models
+{
+    public class Pessoa
+    {
+        public int PessoaID { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+
+        public virtual ICollection<Atividade> Atividades { get; set; }
+    }
+}
